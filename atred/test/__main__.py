1c0976d28d655be254138741fd84e429ec00bcf7
Ali Alizade Haqiqi <alializade@atred.co>
2020-11-24 22:03:34 +0330

    init atred

---
+++ atred/test/__main__.py
@@ -0,0 +1,2 @@
+def launch(data):
+    return data
---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='atred',
+      version='0.0.1',
+      description='Atred',
+      url='http://github.com/storborg/funniest',
+      author='Ali Alizade Haqiqi',
+      author_email='haqaliz@aol.com',
+      license='MIT',
+      packages=['atred'],
+      zip_safe=False)
